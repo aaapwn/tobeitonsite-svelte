4c0fa0c8c5b70c93f056e53f5660bdd65ccf8f9d
aaapwn <armmykung2547@gmail.com>
2023-11-23 02:08:37 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="container">
+	<h1>Hello ToBeIT'67 The Second Onsite</h1>
+	<h3>This is svelte deployment</h3>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
